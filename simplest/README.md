bf86171da4401e0125f06c3d769ab5dcaa89b5df
John Su <su216@hotmail.com>
2020-05-28 20:44:50 -0400

    Add simplest example

---
+++ simplest/README.md
@@ -0,0 +1,3 @@
+# simplest
+
+This is the simplest spring boot project, which only have basic features like controller and model classes.
---
+++ simplest/src/main/java/springboot/examples/simplest/model/Product.java
@@ -0,0 +1,30 @@
+package springboot.examples.simplest.model;
+
+public class Product {
+    private int id;
+    private String name;
+
+    public Product() {
+    }
+
+    public Product(int id, String name) {
+        setId(id);
+        setName(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
